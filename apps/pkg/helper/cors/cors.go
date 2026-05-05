a36a2f2185c40e4eb63cfcde5c3800e4198b7a6a
KiranPawar0 <kiran.pawar@bharatverified.in>
2026-05-05 10:59:48 +0530

    cors 2

--- apps/pkg/helper/cors/cors.go
+++ apps/pkg/helper/cors/cors.go
@@ -1,26 +1,60 @@
 package cors
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/innovativecursor/ixense_kloud_sync_service/apps/pkg/config"
 )
 
 // Helper function to use coors
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
+// func CORSMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
 
+// 		origin := c.Request.Header.Get("Origin")
+
+// 		// Allow only specific origins
+// 		if origin == "http://localhost:3004" || origin == "https://admin-kloudpx.innovativecursor.com" {
+// 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+// 		}
+
+// 		c.Writer.Header().Set("Content-Type", "application/json")
+// 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+// 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+// 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token")
+
+// 		// Handle preflight
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(204)
+// 			return
+// 		}
+
+// 		c.Next()
+// 	}
+// }
+
+func CORSMiddleware() gin.HandlerFunc {
+	cfg, err := config.Env()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
+	}
+
+	allowedOrigins := make(map[string]bool)
+	for _, o := range cfg.Endpoints {
+		allowedOrigins[o] = true
+	}
+
+	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 
-		// Allow only specific origins
-		if origin == "http://localhost:3004" || origin == "https://admin-kloudpx.innovativecursor.com" {
+		if allowedOrigins[origin] {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		}
 
-		c.Writer.Header().Set("Content-Type", "application/json")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-CSRF-Token")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
 
-		// Handle preflight
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
